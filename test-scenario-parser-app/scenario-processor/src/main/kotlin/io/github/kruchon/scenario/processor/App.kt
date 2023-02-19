d3a3ff8ac78dc4760d2e0960f8c48e68c06a68a0
k.kobyshev <k.kobyshev@just-ai.com>
2023-02-20 01:47:00 +0300

    add jaeger

--- test-scenario-parser-app/scenario-processor/src/main/kotlin/io/github/kruchon/scenario/processor/App.kt
+++ test-scenario-parser-app/scenario-processor/src/main/kotlin/io/github/kruchon/scenario/processor/App.kt
@@ -4,8 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class App
-
-fun main(args: Array<String>) {
-    runApplication<App>(*args)
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<App>(*args)
+        }
+    }
 }
+
